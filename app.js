9faf0a481c62e40744e78783eb64b63cca50c4da
githubscientist <sathish.datascientist@gmail.com>
2025-09-27 20:50:34 +0530

    errorRoute, logger, upload --routes --profilePicture, resume endpoints --done

--- app.js
+++ app.js
@@ -2,9 +2,14 @@
 const express = require('express');
 const authRouter = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
+const logger = require('./utils/logger');
+const errorRoute = require('./utils/errorRoute');
 
 // create an express application
 const app = express();
+
+// enable serving static files from the "uploads" directory
+app.use('/uploads', express.static('uploads'));
 
 // middleware to parse JSON request bodies
 app.use(express.json());
@@ -12,8 +17,14 @@ app.use(express.json());
 // middleware to parse cookies
 app.use(cookieParser());
 
+// add the logger middleware
+app.use(logger);
+
 // define the routes -- routers
 app.use('/api/v1/auth', authRouter);
+
+// handle undefined routes
+app.use(errorRoute);
 
 // export the app
 module.exports = app;
